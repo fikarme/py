b176d3583fa9da6b1a5d0e9cf2da031240753e15
fikarme <fikarme@pm.me>
2024-07-26 23:55:04 +0300

    a

--- iter5.py
+++ iter5.py
@@ -1,11 +1,6 @@
-
-
 import random
 from collections import defaultdict
 from itertools import permutations
-
-def get_random_triplets(numbers):
-    return list(permutations(numbers))
 
 def count_adjacent_pairs(triplets):
     pair_counts = defaultdict(int)
@@ -20,42 +15,38 @@ def count_adjacent_pairs(triplets):
             pair_counts[pair] += 1
     return pair_counts
 
-def is_valid_triplet(written_triplets, new_triplet):
-    if written_triplets and written_triplets[-1] == new_triplet:
-        return False
-    if written_triplets and written_triplets[-1][-1] == new_triplet[0]:
-        return False
-    return True
-
-def generate_specific_random_triplets(num_triplets=4):
+def generate_specific_random_triplets():
     numbers = [1, 3, 5]
-    all_triplets = get_random_triplets(numbers)
+    all_triplets = list(permutations(numbers))
+    random.shuffle(all_triplets)
+    
     written_triplets = []
     pair_counts = defaultdict(int)
 
-    while len(written_triplets) < num_triplets:
-        random.shuffle(all_triplets)
-        for new_triplet in all_triplets:
-            if not is_valid_triplet(written_triplets, new_triplet):
-                continue
-            
-            temp_pair_counts = pair_counts.copy()
+    while all_triplets:
+        new_triplet = all_triplets.pop()
+        if written_triplets:
+            last_pair = (written_triplets[-1][-1], new_triplet[0])
+            pair_counts[last_pair] += 1
+
+        for i in range(len(new_triplet) - 1):
+            pair = (new_triplet[i], new_triplet[i + 1])
+            pair_counts[pair] += 1
+        
+        written_triplets.append(new_triplet)
+        
+        # Check if all pairs are balanced
+        counts = list(pair_counts.values())
+        if max(counts) - min(counts) > 1:
+            # If not balanced, revert the addition and shuffle
             for i in range(len(new_triplet) - 1):
                 pair = (new_triplet[i], new_triplet[i + 1])
-                temp_pair_counts[pair] += 1
+                pair_counts[pair] -= 1
             if written_triplets:
                 last_pair = (written_triplets[-1][-1], new_triplet[0])
-                temp_pair_counts[last_pair] += 1
-
-            max_count = max(temp_pair_counts.values())
-            min_count = min(temp_pair_counts.values())
-            if max_count - min_count > 1:
-                continue
-
-            pair_counts = temp_pair_counts
-            written_triplets.append(new_triplet)
-            if len(written_triplets) >= num_triplets:
-                break
+                pair_counts[last_pair] -= 1
+            written_triplets.pop()
+            random.shuffle(all_triplets)
 
     return written_triplets, pair_counts
 
@@ -68,7 +59,6 @@ def print_pair_counts(pair_counts):
 written_triplets, pair_counts = generate_specific_random_triplets()
 
 # Print the generated triplets
-print("Generated Triplets:")
 for triplet in written_triplets:
     print(triplet)
 
